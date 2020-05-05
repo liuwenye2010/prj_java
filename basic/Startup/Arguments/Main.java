75f966fa31ac316e219658544a5df3db483e84b9
johx <johndoe@example.com>
2020-05-05 18:36:59 +0800

    basic starup

---
+++ basic/Startup/Arguments/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello from Java!");
+		//System.out.println(args[0]);//without arg will caue execption
+		
+	}
+
+}
---
+++ basic/Startup/IDEAProject/src/com/example/java/Main.java
@@ -0,0 +1,13 @@
+package com.example.java;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello from IDEA");
+        String  aString ="john";
+        System.out.println("Name is " + aString);
+    }
+}
