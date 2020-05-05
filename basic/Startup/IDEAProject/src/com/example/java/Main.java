9665150c8560b1baf1d9bc34dd8a7a055b8753b9
johx <johndoe@example.com>
2020-05-05 21:24:10 +0800

    add setting of doc shift + F1 to get help from web

--- basic/Startup/IDEAProject/src/com/example/java/Main.java
+++ basic/Startup/IDEAProject/src/com/example/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello from IDEA");
+        System.out.println("Hello from IDEA"); //shift + F1 to get documentation from web , after setting the document path in  platform setting 
         String  aString ="john";
         System.out.println("Name is " + aString);
     }
